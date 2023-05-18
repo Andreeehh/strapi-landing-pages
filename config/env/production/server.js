19c7f9bbe6ca4e9c104e8edc732daf81035190cb
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-05-18 10:10:33 -0300

    render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ( {env}) => ({
   proxy: true,
-  url: env('https://strapi-v4-testss.herokuapp.com/'),
+  url: env('https://strapi-25fx.onrender.com/'),
   app: {
     keys: env.array('APP_KEYS'),
   },
